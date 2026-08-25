dfcefa336c0ba261385027c7928e7394ad58ce7f
Hawk <9399891+nz365guy@users.noreply.github.com>
2026-08-25 12:19:08 +1200

    feat(mail): support matching folder sort order

--- cmd/cb365/mail_list_folder.go
+++ cmd/cb365/mail_list_folder.go
@@ -15,7 +15,22 @@ var (
 	mailListFolderID     string
 	mailListFolderMax    int32
 	mailListFolderFilter string
+	mailListFolderOrder  string
 )
+
+func newMailListFolderQueryParameters() *users.ItemMailFoldersItemMessagesRequestBuilderGetQueryParameters {
+	parameters := &users.ItemMailFoldersItemMessagesRequestBuilderGetQueryParameters{
+		Top:    &mailListFolderMax,
+		Select: []string{"id", "subject", "from", "toRecipients", "receivedDateTime", "lastModifiedDateTime", "isRead", "hasAttachments", "importance", "bodyPreview"},
+	}
+	if order := strings.TrimSpace(mailListFolderOrder); order != "" {
+		parameters.Orderby = []string{order}
+	}
+	if filter := strings.TrimSpace(mailListFolderFilter); filter != "" {
+		parameters.Filter = &filter
+	}
+	return parameters
+}
 
 var mailListFolderCmd = &cobra.Command{
 	Use:   "list-folder",
@@ -38,14 +53,7 @@ var mailListFolderCmd = &cobra.Command{
 		defer cancel()
 
 		reqConfig := &users.ItemMailFoldersItemMessagesRequestBuilderGetRequestConfiguration{
-			QueryParameters: &users.ItemMailFoldersItemMessagesRequestBuilderGetQueryParameters{
-				Top:     &mailListFolderMax,
-				Orderby: []string{"receivedDateTime desc"},
-				Select:  []string{"id", "subject", "from", "toRecipients", "receivedDateTime", "lastModifiedDateTime", "isRead", "hasAttachments", "importance", "bodyPreview"},
-			},
-		}
-		if mailListFolderFilter != "" {
-			reqConfig.QueryParameters.Filter = &mailListFolderFilter
+			QueryParameters: newMailListFolderQueryParameters(),
 		}
 
 		result, err := client.Me().MailFolders().ByMailFolderId(folderID).Messages().Get(ctx, reqConfig)
@@ -90,5 +98,6 @@ func init() {
 	mailListFolderCmd.Flags().StringVar(&mailListFolderID, "folder", "", "Folder ID or well-known folder name")
 	mailListFolderCmd.Flags().Int32Var(&mailListFolderMax, "max", 25, "Maximum messages to return")
 	mailListFolderCmd.Flags().StringVar(&mailListFolderFilter, "filter", "", "OData filter expression")
+	mailListFolderCmd.Flags().StringVar(&mailListFolderOrder, "order-by", "receivedDateTime desc", "OData order-by expression (empty disables ordering)")
 	mailCmd.AddCommand(mailListFolderCmd)
 }
